6a19a2401179e1d647f87a3cc472c4410b82f663
sunsiv <szs0144@auburn.edu>
2020-07-13 18:22:55 -0400

    added Activity Selection

---
+++ Greedy Algorithms/ActivitySelection.scala
@@ -0,0 +1,27 @@
+
+case class Activity(start: Int, end: Int)
+
+object ActivitySelection {
+    def getMaxActivities(activities: Array[Activity]): Int = {
+
+        if(activities.isEmpty) return 0
+
+        val totalActivities = activities.size
+        val activitiesSorted = activities.sortBy(_.end)
+
+        var totalSelectedActivities = 1
+        var currentActivity = activities.head
+        var activityIndex = 1
+        
+        while(activityIndex < totalActivities) {
+            val potentialActivity = activities(activityIndex)
+            if(potentialActivity.start >= currentActivity.end) {
+                totalSelectedActivities += 1
+                currentActivity = potentialActivity
+            }
+            activityIndex += 1
+        }
+
+        return totalSelectedActivities
+    }
+}
