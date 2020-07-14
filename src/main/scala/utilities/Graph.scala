4f61be74eb6419c8d85cf1b507101fbd4165c289
sunsiv <szs0144@auburn.edu>
2020-07-14 18:39:32 -0400

    added graphs

--- src/main/scala/utilities/Graph.scala
+++ src/main/scala/utilities/Graph.scala
@@ -1,16 +1,16 @@
 package utilities
 
-class Node(val id: Any) { 
-    val neighbors = new scala.collection.mutable.MutableList[Node]() 
+class Node[T](val id: T) { 
+    val neighbors = new scala.collection.mutable.MutableList[Node[T]]() 
 }
 
 /* abtract Graph class */
 
-abstract class Graph {
-    val nodes = scala.collection.mutable.Map[Any, Node]()
-    def contains(id: Any):Boolean = nodes.contains(id)
-    def addConnection(id1: Any, id2: Any):Unit
-    def getNode(id: Any):Node = {
+abstract class Graph[T] {
+    val nodes = scala.collection.mutable.Map[T, Node[T]]()
+    def contains(id: T):Boolean = nodes.contains(id)
+    def addConnection(id1: T, id2: T):Unit
+    def getNode(id: T):Node[T] = {
         assume(nodes.contains(id), "node missing")
         return nodes(id)
     }
@@ -18,10 +18,19 @@ abstract class Graph {
 
 /* adjancency list implementation - optimized with Map - optimzed for insert - directed graph */
 
-final class DirectedGraph extends Graph {
-    override def addConnection(id1: Any, id2: Any):Unit = {
+final class DirectedGraph[T] extends Graph[T] {
+    override def addConnection(id1: T, id2: T):Unit = {
         if(!nodes.contains(id1)) nodes += id1 -> new Node(id1)
         if(!nodes.contains(id2)) nodes += id2 -> new Node(id2)
         nodes(id1).neighbors += nodes(id2) 
+    }
+}
+
+final class UndirectedGraph[T] extends Graph[T] {
+    override def addConnection(id_1:T, id_2:T) {
+        if(!nodes.contains(id_1)) nodes += id_1 -> new Node(id_1)
+        if(!nodes.contains(id_2)) nodes += id_2 -> new Node(id_2)
+        nodes(id_1).neighbors += nodes(id_2)
+        nodes(id_2).neighbors += nodes(id_1)
     }
 }
