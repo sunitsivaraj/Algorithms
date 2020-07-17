d1a4a7f8a0000e98f2594411056b3c68cda1d343
sunsiv <szs0144@auburn.edu>
2020-07-16 21:13:43 -0400

    graph changes

--- src/main/scala/greedy/KruskalsMST.scala
+++ src/main/scala/greedy/KruskalsMST.scala
@@ -2,6 +2,8 @@ package greedy
 
 import utilities.{Node, Graph}
 
+// get the minimum weight edge - add to the graph if it does not create a cycle
+
 object Kruskals {
 
 }
--- src/main/scala/utilities/Graph.scala
+++ src/main/scala/utilities/Graph.scala
@@ -1,36 +1,100 @@
 package utilities
 
 class Node[T](val id: T) { 
-    val neighbors = new scala.collection.mutable.MutableList[Node[T]]() 
+    val neighbors = new scala.collection.mutable.ListBuffer[T]() 
 }
 
 /* abtract Graph class */
 
 abstract class Graph[T] {
+
     val nodes = scala.collection.mutable.Map[T, Node[T]]()
     def contains(id: T):Boolean = nodes.contains(id)
-    def addConnection(id1: T, id2: T):Unit
+    def addConnection(id1: T, id2: T): Unit
+    def removeConnection(id1: T, id2: T): Unit
+
     def getNode(id: T):Node[T] = {
         assume(nodes.contains(id), "node missing")
         return nodes(id)
     }
+
+    def isCycle(): Boolean // abstract - as could be optimized for directed graphs
 }
 
 /* adjancency list implementation - optimized with Map - optimzed for insert - directed graph */
 
 final class DirectedGraph[T] extends Graph[T] {
-    override def addConnection(id1: T, id2: T):Unit = {
+
+    import scala.collection.mutable.Map
+    
+    override def addConnection(id1: T, id2: T): Unit = {
         if(!nodes.contains(id1)) nodes += id1 -> new Node(id1)
         if(!nodes.contains(id2)) nodes += id2 -> new Node(id2)
-        nodes(id1).neighbors += nodes(id2) 
+        nodes(id1).neighbors += id2
+    }
+
+    override def removeConnection(id1: T, id2: T): Unit = {
+        nodes(id1).neighbors -= id2
+    }
+
+    override def isCycle(): Boolean = {
+
+        val parent: Map[T, Option[T]]  = Map(nodes.keys.toList.map(x => Tuple2(x, None)):_*)
+
+        def find(id: T): T = {
+            if(parent(id) == None) return id
+            else find(parent(id).get)
+        }
+
+        var cycle = false
+        for(node <- nodes; neighbor <- node._2.neighbors) if(!cycle) {
+            val nodeParent = find(node._1)
+            val neighborParent = find(neighbor)
+            if(nodeParent == neighborParent) cycle = true
+            else parent(nodeParent) = Some(neighborParent) // union operation
+        }
+
+        return cycle
     }
 }
 
 final class UndirectedGraph[T] extends Graph[T] {
+
+    import scala.collection.mutable.Map
+
     override def addConnection(id1: T, id2: T) {
         if(!nodes.contains(id1)) nodes += id1 -> new Node(id1)
         if(!nodes.contains(id2)) nodes += id2 -> new Node(id2)
-        nodes(id1).neighbors += nodes(id2)
-        nodes(id2).neighbors += nodes(id1)
+        nodes(id1).neighbors += id2
+        nodes(id2).neighbors += id1
+    }
+
+    override def removeConnection(id1: T, id2: T): Unit = {
+        nodes(id1).neighbors -= id2
+        nodes(id2).neighbors -= id1
+    }
+
+    override def isCycle(): Boolean = {
+
+        val parent: Map[T, Option[T]] = scala.collection.mutable.Map(nodes.keys.toList.map(x => Tuple2(x, None)):_*)
+        val seen = new scala.collection.mutable.ListBuffer[T] // needed for undirected graphs
+
+        def find(id: T): T = {
+            if(parent(id) == None) return id
+            else find(parent(id).get)
+        }
+
+        var cycle = false
+        for(node <- nodes; neighbor <- node._2.neighbors) if(!cycle && !seen.contains(neighbor)) {
+            val nodeParent = find(node._1)
+            val neighborParent = find(neighbor)
+            if(nodeParent == neighborParent) cycle = true
+            else {
+                parent(nodeParent) = Some(neighborParent) // union operation
+                seen += node._1
+            }
+        }
+
+        return cycle
     }
 }
